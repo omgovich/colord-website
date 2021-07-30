5c5c0cd43be15f1adf5236c4f2449f0121cfe83d
Vlad Shilov <omgovich@ya.ru>
2021-07-30 18:27:43 +0300

    Add OG image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,10 @@ export default class ApplicationDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Unica+One&family=Roboto+Mono&display=swap" rel="stylesheet" />
+          <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+          <meta property="og:image" content="https://colord.omgovich.ru/og.png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
         </Head>
         <body>
           <Main />
