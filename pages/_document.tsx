ae7c5136069dd406d972144fec4b2f901f43aa6b
Vlad Shilov <omgovich@ya.ru>
2021-07-30 18:34:07 +0300

    Add Twitter Card

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,9 +10,15 @@ export default class ApplicationDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Unica+One&family=Roboto+Mono&display=swap" rel="stylesheet" />
           <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+          {/* OG image */}
           <meta property="og:image" content="https://colord.omgovich.ru/og.png" />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="630" />
+          {/* Twitter card */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Colord" />
+          <meta name="twitter:description" content="A tiny yet powerful JavaScript tool for high-performance color manipulations and conversions"/ >
+          <meta name="twitter:image" content="https://colord.omgovich.ru/og.png" />
         </Head>
         <body>
           <Main />
